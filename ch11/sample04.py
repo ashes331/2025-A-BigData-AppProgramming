7cd35e0d0f6d8c766a9728ef71985ac4d3a5c0b8
Sung Won Kim <162572107+ashes331@users.noreply.github.com>
2025-11-14 15:34:58 +0900

    Add files via upload

---
+++ ch11/sample04.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib
+
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
+
+file_name = './data_raw.csv'
+df_raw = pd.read_csv(file_name)
+
+ds_data = df_raw.groupby(['Country']).size()
+
+print('-'*100)
+print(ds_data)
+
+ds_data.nlargest(20).plot.pie(figsize=(10, 10))
+
+plt.title('국가별 데이터 비율')
+plt.tight_layout()
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
