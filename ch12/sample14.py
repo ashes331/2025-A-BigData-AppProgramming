53f7061d71395cd78590c8bf6240c9a3fdd16d16
214 <swkim0503sw@gmail.com>
2025-11-21 15:45:49 +0900

    추가

---
+++ ch12/sample14.py
@@ -0,0 +1,49 @@
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import folium
+from folium.plugins import HeatMap
+from ch12.sample12 import columns
+
+file_name = './data/seoul-metro-2021.logs.csv'
+df_raw = pd.read_csv(file_name)
+
+print('-'*50)
+print(df_raw)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
